6503befaf0a47a903e0e0f066a128aac7642f8df
luke <lukgeneral@gmail.com>
2020-10-21 19:32:56 -0400

    Added yield, init, create

--- luuuuuuuUuUuUuke/a2/sut.c
+++ luuuuuuuUuUuUuke/a2/sut.c
@@ -27,7 +27,10 @@ typedef void (*sut_task_f)();
 /* Queue data */
 
 // Ready task queue
-struct queue *task_queue;
+struct queue* task_queue;
+
+// IO wait queue
+struct queue* wait_queue;
 
 // A task is an entry in the queue
 struct queue_entry;
@@ -44,6 +47,9 @@ pthread_t i_thread_handle;
 pthread_mutex_t c_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t i_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// Flags
+bool is_shutdown = false;
+
 // C-EXEC function
 void *thread_c(){
 
@@ -54,6 +60,8 @@ void *thread_c(){
 // Context switching can be used for user-level threads
 
 ucontext_t m; //main context
+
+int numberThreads;
 
 // I-EXEC function
 void *thread_i(){
@@ -66,16 +74,73 @@ void *thread_i(){
 
 void sut_init(){
 
+    // Thread counter
+    numberThreads = 0;
+
+    // Create queue
+    task_queue = malloc(sizeof(struct queue));
+    *task_queue = queue_create();
+    // Initialize queue
+    queue_init(task_queue);
+
+    wait_queue = malloc(sizeof(struct queue));
+    *wait_queue = queue_create();
+    queue_init(wait_queue);
+
     // Creating kernel-level threads
     //pthread_create(&c_thread_handle,NULL, thread_c, NULL);
     //pthread_create(&i_thread_handle,NULL, thread_i, NULL);
+    printf("init success\n");
 }
 
+/**
+ *  Takes in a C function as argument and creates new task added 
+ *  to the end of the ready queue.
+ */ 
 bool sut_create(sut_task_f fn){
 
+    if(numberThreads > MAX_THREADS){
+        printf("Error: Limit of created threads has been reached.\n");
+        return false;
+    }else{
+        // Create new task by initializing struct type
+        struct task *new_task = malloc(sizeof(struct task));
+        new_task -> task_id = numberThreads;
+        new_task -> task_func = fn;
+        new_task -> task_context.uc_stack.ss_sp = new_task -> task_stack;
+        new_task -> task_context.uc_stack.ss_size = THREAD_STACK_SIZE;
+        new_task -> task_context.uc_link = 0;
+        makecontext(&(new_task -> task_context), fn, 0);
+        numberThreads++;
+
+        // Add task to back of ready queue
+        queue_insert_tail(task_queue, queue_new_node(new_task));
+
+        printf("created new task is a success\n");
+        return true;
+    }
 }
 
+/**
+ * Pauses execution of a thread until scheduler selects it again. 
+ * The task is placed at back of ready queue for further execution.  
+ */
+
 void sut_yield(){
+    struct task* current_task;
+    current_task = queue_pop_head(task_queue) ->data;
+
+    // When we swap tasks, the context must be saved before in TCB
+    getcontext(&(current_task->task_context));
+    current_task -> task_context.uc_stack.ss_sp = current_task -> task_stack;
+    current_task -> task_context.uc_stack.ss_size = THREAD_STACK_SIZE;
+    current_task -> task_context.uc_link = &m;
+
+    // Add back to ready queue at back
+    queue_insert_tail(task_queue, queue_new_node(current_task));
+
+    // Swap contexts
+    swapcontext(&(current_task->task_context),&m);
 
 }
 
@@ -100,5 +165,5 @@ char *sut_read(){
 }
 
 void sut_shutdown(){
-
+    
 }
